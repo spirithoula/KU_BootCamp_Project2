016aaffb77335e5c2f4c47b4314cbf0025b7f2a9
Travis Brown <bergannation@gmail.com>
2021-06-28 19:40:11 -0500

    fixing seed files

--- public/js/seeds.js
+++ public/js/seeds.js
@@ -1,36 +1,20 @@
 var locations = [
     {
-      name: "Barker Field",
-      lat: 37.5391116,
-      lon: -77.4842119
+      name: "Test Location",
+      lat: 39.099792,
+      lon: -94.57856
     },
     {
-      name: "Churchill Dog Park",
-      lat: 37.5239282,
-      lon: -77.4126637
-    },
-    {
-      name: "Phideaux Dog Park",
-      lat: 37.5198621,
-      lon: -77.483193
-    },
-    {
-      name: "Northside Dog Park",
-      lat: 37.5977001,
-      lon: -77.4436564
-    },
-    {
-      name: "Rockwood Park",
-      lat: 37.4469508,
-      lon: -77.5840857
+      name: "Test Location 2",
+      lat: 39.099792,
+      lon: -94.57856
     }
+    
   ];
   
   //seed button for parks to facilitate easier initial deployment on heroku and elsewhere
   $("#parkSeeds").click(function(){
-    $.get("/api/users/location/", result => {
-      console.log(result);
-     //a check to make sure you haven't already clicked the button and double seeded the table
+   
         console.log("Location Seeds being added");
         locations.forEach( location => {
           $.post("/api/users/location/seeds",{
@@ -41,5 +25,4 @@ var locations = [
           console.log(response);
           });
         })
-    })
   });//end of seed click
