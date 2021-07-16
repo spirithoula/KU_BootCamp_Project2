0d74526f21896ad747a0b8c90bd4331129ef9333
Alex Harrington <spirithoula2@gmail.com>
2021-07-15 22:54:18 -0500

    tried to add emailjs contact form to routes

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -463,5 +463,15 @@ router.get("/location/:id?", (req, res) => {
   }
 }); //end of location get
 
+var templateParams = {
+  name: 'name',
+  notes: 'message'
+};
+emailjs.sendForm("user_k9C8gatxe5lmVjewlqf8N", 'contact_form', '#myForm')
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
 
 module.exports = router;
